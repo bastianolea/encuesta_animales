e2c5505c744d553a3c08f8c79563a68330e0c34b
Pingfan Hu <pingfan0727@gmail.com>
2024-09-06 11:18:27 -0400

    sd_database() moved to setup

--- server.R
+++ server.R
@@ -1,25 +1,3 @@
-
-##################
-# database setup #
-##################
-
-# This demo requires an active Supabase connection to run properly!
-
-# surveydown stores data on a database that you define at https://supabase.com/
-# To connect to a database to store the survey data, update this with details
-# from your supabase account and database. You can also set pause = TRUE
-# which will ignore the other settings and won't attempt to connect to the
-# database. See documentation for details at https://surveydown.org/store-data
-
-db <- sd_database(
-  host   = "",
-  dbname = "",
-  port   = "",
-  user   = "",
-  table  = "",
-  pause  = TRUE
-)
-
 ################
 # config setup #
 ################
