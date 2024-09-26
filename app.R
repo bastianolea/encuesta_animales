ce7d4630879d26e4a5350e9eaa58da29f13de041
John Helveston <john.helveston@gmail.com>
2024-09-26 10:58:22 -0400

    update to latest `sd_get_data()` with refresh_interval

--- app.R
+++ app.R
@@ -5,12 +5,9 @@ library(ggplot2)
 
 # Database setup
 
-# surveydown stores data on a database that you define at https://supabase.com/
-# To connect to a database, update the sd_database() function with details
-# from your supabase database. For this demo, we set ignore = TRUE, which will
-# ignore the settings and won't attempt to connect to the database. This is
-# helpful for local testing if you don't want to record testing data in the
-# database table. See the documentation for details:
+# For this demo, you need to create a database at https://supabase.com/
+# and connect to it using the sd_database() function with details
+# from your supabase database. See the documentation for details:
 # https://surveydown.org/store-data
 
 db <- sd_database(
@@ -18,15 +15,14 @@ db <- sd_database(
     dbname = "",
     port   = "",
     user   = "",
-    table  = "",
-    ignore = TRUE
+    table  = ""
 )
 
 # Server setup
 server <- function(input, output, session) {
 
-    # Get data in 5 sec interval
-    data <- sd_get_data(db)
+    # Refresh data every 5 seconds
+    data <- sd_get_data(db, refresh_interval = 5)
 
     # Render the plot
     output$penguin_plot <- renderPlot({
